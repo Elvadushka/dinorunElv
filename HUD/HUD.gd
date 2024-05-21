596a35a6832a7dd16ff3da67263d676c4e0a1f58
Elvadushka <elimariana16@gmail.com>
2024-05-21 13:56:45 -0600

    mensaje

---
+++ HUD/HUD.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+var Score=0
+var Highscore=0
+var is_playing=false
+
+func update_Score_label():
+	Score=Score + 100
+	$ScoreLabel.text ="Score: %d"%(Score)
+
+
+func update_Higscore_lable():
+	if Score > Highscore:
+		Highscore=Score
+		$ScoreLabel.text="Higscore: %d"%(Highscore)
+		
+func _on_puchale_play_pressed():
+	is_playing=true
+	$Scoretimer.start()
+	$"Puchale play".visible=false
+	
+func _on_scoretimer_timeout():
+	update_Score_label() 
+
---
+++ main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var screen_size : Vector2i
+var PlayerInitialPosition
+
+func _ready():
+	screen_size = get_window().size
+	PlayerInitialPosition = $PlayerElv.position
+
+func new_game():
+	$PlayerElv.position = PlayerInitialPosition
+	$Floor.position.x = 0
+	
+
+func game_over():
+	GHUD.update_Higscore_lable() 
+
+func _process(delta):
+	if ($PlayerElv.position.x-$Floor.position.x)> screen_size.x:
+		$Floor.position.x =$Floor.position.x + screen_size.x
